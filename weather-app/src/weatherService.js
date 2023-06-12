c42cbb6d7cb300e84e1400163c022e2b92add693
vaishnavisk1212 <129852316+vaishnavisk1212@users.noreply.github.com>
2023-06-12 21:51:37 +0530

    Add files via upload

---
+++ weather-app/src/weatherService.js
@@ -0,0 +1,37 @@
+const API_KEY = "c64c2113fc5c0033c890ffd680ccdd12";
+
+const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getFormattedData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_max,
+    temp_min,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getFormattedData };
